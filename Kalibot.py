ba825e046cad8b88cfe8de4da3bb63f4af4598f1
Damrey Rizon <148986553+damreykalicube@users.noreply.github.com>
2024-02-27 19:15:11 +0800

    Update Kalibot.py

--- Kalibot.py
+++ Kalibot.py
@@ -12,7 +12,7 @@ from github import Github
 st.set_page_config(page_title="Kalibot", page_icon="🤖")
 
 st.title("Kalibot - Kalicube Pro Version")
-git_tok = Github(os.environ["github_key2"])
+#git_tok = Github(os.environ["github_key2"])
 openai.api_key = os.environ["openai_secret_key"]
 pinecone.init(
     api_key= os.environ["pinecone_secret_key"],  # app.pinecone.io (console)
